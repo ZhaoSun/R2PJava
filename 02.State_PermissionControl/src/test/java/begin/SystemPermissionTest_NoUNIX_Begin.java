8f81d366ae5c9f7e5d51fc90c741d084dcfabfbf
ZhaoSun <sunzhao199@gmail.com>
2021-04-10 10:07:52 +0800

    step2 after create base class, change string "state" to object state; fix compile error then unit test

--- 02.State_PermissionControl/src/test/java/begin/SystemPermissionTest_NoUNIX_Begin.java
+++ 02.State_PermissionControl/src/test/java/begin/SystemPermissionTest_NoUNIX_Begin.java
@@ -24,19 +24,19 @@ public class SystemPermissionTest_NoUNIX_Begin {
         // 直接 Claim 操作
         // 结果：CLAIMED 状态， isGranted = false
         permission.claimedBy(admin);
-        assertEquals("claimed", permission.CLAIMED, permission.state());
+        assertEquals("claimed", PermissionState.CLAIMED, permission.getState());
         assertEquals("claimed", false, permission.isGranted());
 
         // 先 Claim 再进行 Grant 操作，
         // 结果：GRANTED 状态， isGranted = true
         permission.grantedBy(admin);
-        assertEquals("granted", permission.GRANTED, permission.state());
+        assertEquals("granted", PermissionState.GRANTED, permission.getState());
         assertEquals("granted", true, permission.isGranted());
 
         // 再进行 Claim 操作
         // 结果：GRANTED 状态， isGranted = true
         permission.claimedBy(admin);
-        assertEquals("granted", permission.GRANTED, permission.state());
+        assertEquals("granted", PermissionState.GRANTED, permission.getState());
         assertEquals("granted", true, permission.isGranted());
     }
 
@@ -45,14 +45,14 @@ public class SystemPermissionTest_NoUNIX_Begin {
         // 未 Claim 的状态进行 Denied 操作，
         // 结果：REQUESTED 状态， isGranted = false
         permission.grantedBy(admin);
-        assertEquals("requested", permission.REQUESTED, permission.state());
+        assertEquals("requested", PermissionState.REQUESTED, permission.getState());
         assertEquals("not granted", false, permission.isGranted());
 
         // 先 Claim 再进行 Denied 操作，
         // 结果：DENIED 状态， isGranted = false
         permission.claimedBy(admin);
         permission.deniedBy(admin);
-        assertEquals("granted", permission.DENIED, permission.state());
+        assertEquals("granted", PermissionState.DENIED, permission.getState());
         assertEquals("granted", false, permission.isGranted());
     }
 
@@ -61,14 +61,14 @@ public class SystemPermissionTest_NoUNIX_Begin {
         // 未 Claim 的状态进行 Grant 操作，
         // 结果：REQUESTED 状态， isGranted = false
         permission.grantedBy(admin);
-        assertEquals("requested", permission.REQUESTED, permission.state());
+        assertEquals("requested", PermissionState.REQUESTED, permission.getState());
         assertEquals("not granted", false, permission.isGranted());
 
         // 先 Claim 再进行 Grant 操作，
         // 结果：GRANTED 状态， isGranted = true
         permission.claimedBy(admin);
         permission.grantedBy(admin);
-        assertEquals("granted", permission.GRANTED, permission.state());
+        assertEquals("granted", PermissionState.GRANTED, permission.getState());
         assertEquals("granted", true, permission.isGranted());
     }
 }
